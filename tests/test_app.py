4eb77215f565e4b925ef48b522b19eefe4f5bea4
Victor Contreras <contreras3991@gmail.com>
2024-08-23 21:28:57 -0300

    tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_madr.app import app
-
-
-def test_read_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
-
+def test_read_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/contas/',
+        json={
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'testtest'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+            'id': 1,
+            'username': 'test',
+            'email': 'test@test.com',
+        }
---
+++ tests/test_db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select
+
+from fastapi_madr.models import Account
+
+
+def test_create_account(session):
+    new_account = Account(
+        username='victor',
+        email='victor@contreras.com',
+        password='teste'
+    )
+
+    session.add(new_account)
+    session.commit()
+    session.refresh(new_account)
+
+    account = session.scalar(select(Account).where(Account.username == 'victor'))
+
+    assert account.username == 'victor'

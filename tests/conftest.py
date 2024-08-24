202ad1fc89369b63b1834afc8072b79a10271108
Victor Contreras <contreras3991@gmail.com>
2024-08-24 12:03:00 -0300

    inserted get_session_overrides functions in client fixture to run tests with sqlite database in memory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,33 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import Session
 
 from fastapi_madr.app import app
+from fastapi_madr.database import get_session
 from fastapi_madr.models import table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_overrides():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_overrides
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool
+    )
+
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:

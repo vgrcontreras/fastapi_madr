9d7f0292230dd4cd5df8707fe25f63801c948831
Victor Contreras <contreras3991@gmail.com>
2024-08-23 21:26:35 -0300

    modulo para inicialização da aplicação

--- fastapi_madr/app.py
+++ fastapi_madr/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from fastapi_madr.routers import contas
+from fastapi_madr.schemas import Message
+
 app = FastAPI()
 
+app.include_router(contas.router)
 
-@app.get('/')
+
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
